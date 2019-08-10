537f3a7bb51926c97599e3168e185c4ba773d3ba
王先硚 <709922234@qq.com>
2019-08-10 13:53:51 +0800

    feat: use service

--- src/store/posts.ts
+++ src/store/posts.ts
@@ -1,12 +1,18 @@
 import { createStore, updateStore } from '@mantou/gem';
+import * as api from '../service/api';
+import { Post } from '../service/api';
 
-export const posts = createStore({ list: [], loading: false });
+interface PostStore {
+  list: Post[];
+  loading: boolean;
+}
+
+export const posts = createStore<PostStore>({ list: [], loading: false });
 
 export const fetchPosts = async () => {
   updateStore(posts, { loading: true });
   try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const list = await res.json();
+    const list = await api.getPosts();
     await new Promise(resolve => setTimeout(resolve, 1000));
     updateStore(posts, { list });
   } finally {
